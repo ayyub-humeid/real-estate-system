778a0088424fce9d209fb61671793ac056a60a1d
ayyub humeid <ayoubhumeid@gmail.com>
2026-08-06 12:34:20 +0300

    add all links to avoid cors issues

--- config/cors.php
+++ config/cors.php
@@ -20,7 +20,8 @@ return [
         'http://localhost:5173',
         'http://10.10.10.35:3000',
         'https://real-state-frontend-with-nextjs-meo.vercel.app',
-        'https://real-state-frontend-with-nextjs.vercel.app',
+        'https://real-state-frontend-with-nex-git-b2de8e-ayyub-humeid-s-projects.vercel.app',
+        'https://real-state-frontend-with-nextjs-meoj-o9yen8oeg.vercel.app'
     ],
 
     'allowed_origins_patterns' => [],
